668547b8cf601d23f847658a48d746a21b114372
魔神翼 <power721@163.com>
2019-11-28 10:50:26 +0800

    update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt install curl gnupg2 ca-certificates lsb-release
+sudo apt -y install curl gnupg2 ca-certificates lsb-release
 echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
     | sudo tee /etc/apt/sources.list.d/nginx.list
 curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 sudo apt-key fingerprint ABF5BD827BD9BF62
 sudo apt update
-sudo apt install nginx
+sudo apt install -y nginx
+sudo systemctl start nginx
+sudo systemctl status nginx
