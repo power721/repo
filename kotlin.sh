469158cf20d9fafa0047eaf7026f0c43477cf781
魔神翼 <power721@163.com>
2019-11-28 14:39:08 +0800

    add kotlin.sh

---
+++ kotlin.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+VERSION=1.3.60
+PRIORITY=100
+
+sudo apt install unzip
+
+wget -O /tmp/kotlin.zip https://github.com/JetBrains/kotlin/releases/download/v${VERSION}/kotlin-compiler-${VERSION}.zip
+sudo unzip /tmp/kotlin.zip -d /usr/lib
+sudo chown -R root:root /usr/lib/kotlinc
+
+sudo update-alternatives --install /usr/bin/kotlin kotlin /usr/lib/kotlinc/bin/kotlin ${PRIORITY}
+sudo update-alternatives --install /usr/bin/kotlinc kotlinc /usr/lib/kotlinc/bin/kotlinc ${PRIORITY}
+
+kotlin -version
+kotlinc -version
