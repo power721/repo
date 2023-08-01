31be34984aa5e82441a08a8214ce9207112d1763
魔神翼 <power721@163.com>
2023-08-01 13:33:12 +0000

    update go.sh.
    Signed-off-by: 魔神翼 <power721@163.com>

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=1.19
+VERSION=1.20.6
 PRIORITY=100
 
 wget -O /tmp/go.tgz --no-check-certificate https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz
@@ -9,7 +9,7 @@ sudo chown -R root:root /usr/lib/go
 
 sudo update-alternatives --install /usr/bin/go go /usr/lib/go/bin/go ${PRIORITY}
 
-echo "export GOROOT=/usr/lib/go" >/tmp/go-env.sh
+#echo "export GOROOT=/usr/lib/go" >/tmp/go-env.sh
 sudo cp /tmp/go-env.sh /etc/profile.d/
 sudo chmod 444 /etc/profile.d/go-env.sh
 
