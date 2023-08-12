ba0433e41a770fd591026c48d324c41d3b64dc62
Harold <power0721@gmail.com>
2023-08-12 12:55:00 +0800

    update node.sh

--- node.sh
+++ node.sh
@@ -9,23 +9,24 @@ fi
 VERSION=18.17.0
 PRIORITY=100
 
+ARCH=x64
 if [ "$platform" = "aarch64" ]; then
-  wget -O /tmp/node.tgz https://nodejs.org/dist/v${VERSION}/node-v${VERSION}-linux-arm64.tar.xz
-else
-  wget -O /tmp/node.tgz https://nodejs.org/dist/v${VERSION}/node-v${VERSION}-linux-x64.tar.xz
+	ARCH=arm64
 fi
-
+wget -O /tmp/node.tgz https://nodejs.org/dist/v${VERSION}/node-v${VERSION}-linux-${ARCH}.tar.xz
 sudo tar xf /tmp/node.tgz -C /usr/lib
-sudo chown -R root:root /usr/lib/node-v${VERSION}-linux-x64
+sudo chown -R root:root /usr/lib/node-v${VERSION}-linux-${ARCH}
 
-sudo update-alternatives --install /usr/bin/node node /usr/lib/node-v${VERSION}-linux-x64/bin/node ${PRIORITY}
-sudo update-alternatives --install /usr/bin/npm npm /usr/lib/node-v${VERSION}-linux-x64/bin/npm ${PRIORITY}
+sudo update-alternatives --install /usr/bin/node node /usr/lib/node-v${VERSION}-linux-${ARCH}/bin/node ${PRIORITY}
+sudo update-alternatives --install /usr/bin/npm npm /usr/lib/node-v${VERSION}-linux-${ARCH}/bin/npm ${PRIORITY}
 
-echo "export NODE_HOME=/usr/lib/node-v${VERSION}-linux-x64" >/tmp/node-env.sh
-echo "export PATH=/usr/lib/node-v${VERSION}-linux-x64/bin:\$PATH" >>/tmp/node-env.sh
+echo "export NODE_HOME=/usr/lib/node-v${VERSION}-linux-${ARCH}" >/tmp/node-env.sh
+echo "export PATH=/usr/lib/node-v${VERSION}-linux-${ARCH}/bin:\$PATH" >>/tmp/node-env.sh
 
 sudo cp /tmp/node-env.sh /etc/profile.d/
 sudo chmod 444 /etc/profile.d/node-env.sh
+
+rm -f /tmp/node.tgz
 
 echo -n "node version: " && node -v
 echo -n "npm  version: " && npm -v
