1dac43c41c59ce37f41a19d38a431b9f84cec106
魔神翼 <power721@163.com>
2024-04-11 01:49:55 +0000

    update jdk17.sh.
    Signed-off-by: 魔神翼 <power721@163.com>

--- jdk17.sh
+++ jdk17.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add - 
-sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
+wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
 
 sudo apt-get update
 sudo apt-get install -y java-17-amazon-corretto-jdk
