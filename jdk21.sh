c85ce07c850e34d53d1190f0ee87935a3505873e
power721 <power0721@gmail.com>
2026-03-17 11:33:40 +0800

    Create jdk21.sh

---
+++ jdk21.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# https://docs.aws.amazon.com/corretto/latest/corretto-17-ug/generic-linux-install.html
+
+wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+
+sudo apt-get update
+sudo apt-get install -y java-21-amazon-corretto-jdk
+
+java -version
+javac -version
